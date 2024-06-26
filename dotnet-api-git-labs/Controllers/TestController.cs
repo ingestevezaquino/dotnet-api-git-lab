e22dcfcb1429c13e276b2b9970a7594d1836e13b
Jesus Estevez <jestevez.personal@gmail.com>
2024-06-25 22:05:42 -0400

    add TestController.cs file

---
+++ dotnet-api-git-labs/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_api_git_labs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok("Test funciona correctamente!");
+        }
+    }
+}
